2d0964376a945906eb33c30e0550c05a3171869b
Ferran <ferranpadroramos@gmail.com>
2026-03-04 19:28:58 +0100

    header

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>
+          
+        </header>
         {children}
       </body>
     </html>
