280364b514125a3dc0271d6aea0fcac890605bb1
Ferran <ferranpadroramos@gmail.com>
2026-04-28 17:32:18 +0200

    proxy

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import "dotenv/config"
-import { PrismaClient } from "../app/generated/prisma/client"
+import { PrismaClient } from "@/app/generated/prisma/client"
 import { PrismaPg } from "@prisma/adapter-pg"
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
